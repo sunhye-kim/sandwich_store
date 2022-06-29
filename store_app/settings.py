a332c5a23e5fbda7e99d3584afc83f183ae89cd1
sunwise <sunwise9634@gmail.com>
2022-06-29 23:20:43 +0900

    DEL : 키제거

--- store_app/settings.py
+++ store_app/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ih9uv3)=bop=joi&phoss!8rb8m+e*xh%vmfv%3*bxs&hmnx^r'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
